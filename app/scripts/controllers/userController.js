905cf9107d25a43e3886829a63314fbc052438d5
Ryan Chambers <ryan.b.chambers@gmail.com>
2014-07-15 08:23:43 -0400

    add confirmation for delete

--- app/scripts/controllers/userController.js
+++ app/scripts/controllers/userController.js
@@ -1,15 +1,27 @@
 App.UserController = Ember.ObjectController.extend({
+  // the deleteMode property is false by default
+  deleteMode: false,
+
   actions: {
-    edit: function(){
+    edit: function() {
       this.transitionToRoute('user.edit');
     },
 
-	"delete" : function(){
+	"delete" : function() {
+		this.toggleProperty('deleteMode');
+	},
+
+    cancelDelete: function(){
+      // set deleteMode back to false
+      this.set('deleteMode', false);
+    },
+
+	confirmDelete : function() {
 	    // this tells Ember-Data to delete the current user
 	    this.get('model').deleteRecord();
 	    this.get('model').save();
 	    // then transition to the users route
 	    this.transitionToRoute('users');
-	  }
+	}
   }
 });
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -35,8 +35,18 @@ function program1(depth0,data) {
 this["Ember"]["TEMPLATES"]["user"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', stack1, escapeExpression=this.escapeExpression;
+  var buffer = '', stack1, escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = '';
+  data.buffer.push("\n<div class=\"confirm-box\">\n  <h4>Really?</h4>\n  <button ");
+  data.buffer.push(escapeExpression(helpers.action.call(depth0, "confirmDelete", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data})));
+  data.buffer.push("> yes </button>\n  <button ");
+  data.buffer.push(escapeExpression(helpers.action.call(depth0, "cancelDelete", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data})));
+  data.buffer.push("> no </button>\n</div>\n");
+  return buffer;
+  }
 
   data.buffer.push("<div class=\"user-profile\">\n  <button ");
   data.buffer.push(escapeExpression(helpers.action.call(depth0, "edit", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data})));
@@ -60,6 +70,9 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("</span>\n</div>\n\n");
   stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n\n");
+  stack1 = helpers['if'].call(depth0, "deleteMode", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   return buffer;
   
